d789e3f95b2a36b814ecda707f4479fdf9ce392c
deshanWimalasooriya <deshanwimalasooriya000@gmail.com>
2025-05-14 20:11:41 +0530

    Create Email.java

---
+++ emailCheck/Email.java
@@ -0,0 +1,18 @@
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Email{
+    public static void main(String args[]){
+
+        String text = "hi i'm deshan wimamlasooriya, deshan@gmail.com, and my friend is dilanka, dilanka123@gmail.com";
+        String regex = "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-z]{2,6}";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+       
+        while(matcher.find()){
+            System.out.println(matcher.group());;
+        }
+    }
+}
