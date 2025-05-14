273c1ffbb3971db4ebc57deb7aafe85cd8558ad2
deshanWimalasooriya <deshanwimalasooriya000@gmail.com>
2025-05-14 16:56:34 +0530

    add

---
+++ collection/Main.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String args[]){
+
+        //Create a ArrayList
+        ArrayList<Integer> arr = new ArrayList<Integer>(10);
+
+        arr.add(10);
+        arr.add(14);
+
+        System.out.println(arr);
+
+    }
+}
